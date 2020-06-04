7a14e479fd2388c4135e6849cd6a18563b6afadb
anastasiabespalova <32164656+anastasiabespalova@users.noreply.github.com>
2020-06-04 23:01:54 +0300

    Add files via upload

---
+++ task22/instructions.sh
@@ -0,0 +1,3 @@
+python attacks.py --e 1073780833 --N 1220275921 --out_file my_report.txt
+
+python attacks.py --test_regime --attack_time 1 --complexity 16 --attacks Pollard_p_1,Pollard_rho,Wiener,Minor_mod,Cyclic
